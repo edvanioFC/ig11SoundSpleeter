538abc58ce2f45ba8260194128bf4799d31d42eb
edvaniofc <edvaniocorreiacorreia@gmail.com>
2024-08-06 08:17:55 +0100

    improving the back

--- server/app.py
+++ server/app.py
@@ -65,15 +65,6 @@ def process_audio():
         output_path = os.path.join(app.config['OUTPUT_FOLDER'], unique_id)
         os.makedirs(output_path, exist_ok=True)
         separator.separate_to_file(file_path, output_path)
-
-        '''# Caminhos para os arquivos processados
-        vocals_filename = os.path.join(output_path, 'vocals.wav')
-        accompaniment_filename = os.path.join(output_path, 'accompaniment.wav')
-
-        # acessar os arquivos dentro do diretório unique_id/unique_id_unique_filename/vocal.wav...
-        vocals_output_path = os.path.join(unique_filename, 'vocals.wav')
-        accompaniment_output_path = os.path.join(unique_filename, 'accompaniment.wav')
-        os.makedirs(os.path.join(output_path, unique_filename), exist_ok=True)'''
                 
         print('Unicoooo:', unique_filename)
 
